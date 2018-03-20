5845e5e22e437c767c2bf0a9d0eff91cdb448fef
sorinsarca <sarca_sorin@hotmail.com>
2018-03-20 14:59:25 +0200

    Update

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+import Vue from "vue";
 import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
 import VuetifyJsonForm from '@aquarelle/vuetify-json-form';
-import {DOMPortal, User, EntityTypeControl, EntityInstanceControl} from "./src"
+import {DOMPortal, User, EntityTypeControl, EntityInstanceControl, Router, AppRoot} from "./src"
 
-export default function (Vue) {
-    Vue.use(DOMPortal);
-    Vue.use(VueRouter);
-    Vue.use(Vuetify);
-    Vue.use(VuetifyJsonForm);
-    Vue.use(EntityTypeControl);
-    Vue.use(EntityInstanceControl);
-    Vue.prototype.$user = User;
-    Vue.directive('title', {
-        inserted: (el, binding) => document.title = binding.value,
-        update: (el, binding) => document.title = binding.value
-    });
-}
+Vue.use(DOMPortal);
+Vue.use(VueRouter);
+Vue.use(Vuetify);
+Vue.use(VuetifyJsonForm);
+Vue.use(EntityTypeControl);
+Vue.use(EntityInstanceControl);
+Vue.directive('title', {
+    inserted: (el, binding) => document.title = binding.value,
+    update: (el, binding) => document.title = binding.value
+});
+Vue.prototype.$user = User;
+
+export * from "./src";
+export default (new Vue({
+    el: '#app',
+    router: Router,
+    render(h) {
+        return h(AppRoot)
+    }
+}));
