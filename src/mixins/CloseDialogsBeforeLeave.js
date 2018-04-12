14002fd00e2243090c5d34c2edd7b1d247541fa9
sorinsarca <sarca_sorin@hotmail.com>
2018-04-12 19:09:18 +0300

    Updated

--- src/mixins/CloseDialogsBeforeLeave.js
+++ src/mixins/CloseDialogsBeforeLeave.js
@@ -1,18 +1,78 @@
+function closeFormDialogs(control)
+{
+    if (!control || typeof control !== 'object') {
+        return true;
+    }
+
+    if (Array.isArray(control)) {
+        for (let i = 0; i < control.length; i++) {
+            if (closeFormDialogs(control) === false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if (!control.$refs) {
+        return true;
+    }
+
+    const formName = control.formName || 'form';
+    const blockForm = control.$refs[formName];
+
+    if (!blockForm || !blockForm.$refs) {
+        return true;
+    }
+
+    const dialogsWrapper = blockForm.$refs.dialogs;
+    if (!dialogsWrapper || !dialogsWrapper.dialogs) {
+        return true;
+    }
+
+    const length = dialogsWrapper.dialogs.length;
+    if (length > 0) {
+        dialogsWrapper.onCancel(dialogsWrapper.dialogs[length - 1]);
+        return false;
+    }
+
+    return true;
+}
+
+function doOnRouteLeave(control)
+{
+    if (!control || typeof control !== 'object') {
+        return true;
+    }
+
+    if (Array.isArray(control)) {
+        for (let i = 0; i < control.length; i++) {
+            if (doOnRouteLeave(control) === false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if (typeof control.onRouteLeave === 'function') {
+        return control.onRouteLeave(doOnRouteLeave, closeFormDialogs);
+    }
+
+    return true;
+}
+
 export default {
     beforeRouteLeave(to, from, next) {
-        const formName = this.formName || 'form';
-        if (!this.$refs[formName] || !this.$refs[formName].$refs || !this.$refs[formName].$refs.dialogs) {
-            next();
-            return;
-        }
-        const dialogsWrapper = this.$refs.form.$refs.dialogs;
-        const length = dialogsWrapper.dialogs.length;
-        if (length > 0) {
-            dialogsWrapper.onCancel(dialogsWrapper.dialogs[length - 1]);
+        if (doOnRouteLeave(this) === false) {
             next(false);
+            return false;
         }
-        else {
-            next();
+
+        if (closeFormDialogs(this) === false) {
+            next(false);
+            return false;
         }
+
+        next();
+        return true;
     }
 }
