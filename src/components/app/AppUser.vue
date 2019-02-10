4de047885fa4af4c336855e00ab8a26987ffaf0e
Marius Sarca <marius.sarca@gmail.com>
2019-02-10 22:33:24 +0200

    Use menu

--- src/components/app/AppUser.vue
+++ src/components/app/AppUser.vue
@@ -9,9 +9,33 @@
                 <v-list-tile-sub-title>{{user.email}}</v-list-tile-sub-title>
             </v-list-tile-content>
             <v-list-tile-action>
-                <v-btn icon @click.prevent.stop="signOut">
-                    <v-icon>exit_to_app</v-icon>
-                </v-btn>
+                <v-menu>
+                    <v-btn icon slot="activator">
+                        <v-icon>arrow_drop_down</v-icon>
+                    </v-btn>
+                    <v-list>
+                        <v-list-tile @click="">
+                            <v-list-tile-avatar>
+                                <v-icon>person</v-icon>
+                            </v-list-tile-avatar>
+                            <v-list-tile-content>
+                                <v-list-tile-title>My account</v-list-tile-title>
+                            </v-list-tile-content>
+                            <v-list-tile-action></v-list-tile-action>
+                        </v-list-tile>
+                        <v-divider></v-divider>
+                        <v-list-tile @click.prevent.stop="signOut">
+                            <v-list-tile-avatar>
+                                <v-icon>exit_to_app</v-icon>
+                            </v-list-tile-avatar>
+                            <v-list-tile-content>
+                                <v-list-tile-title>Sign out</v-list-tile-title>
+                            </v-list-tile-content>
+                            <v-list-tile-action></v-list-tile-action>
+                        </v-list-tile>
+                    </v-list>
+                </v-menu>
+
             </v-list-tile-action>
         </v-list-tile>
         <v-divider></v-divider>
